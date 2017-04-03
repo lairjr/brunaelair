65c76dd3eeff508594e4f0a766fcc51f6007e2cd
Lair Junior <lairjr@outlook.com>
2017-04-03 20:51:55 -0300

    Adjust bulma css in angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', '../../node_modules/bulma/css/bulma.css']
+  styleUrls: ['./app.component.css', ]
 })
 export class AppComponent { }
