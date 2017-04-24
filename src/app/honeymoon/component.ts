b18734c9dd783ddf96a681c3e9f210a63fc5b91b
lairjr <lairjr@outlook.com>
2017-04-24 17:20:08 -0300

    Add logic on honeymoon page

--- src/app/honeymoon/component.ts
+++ src/app/honeymoon/component.ts
@@ -3,4 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   templateUrl: './page.html'
 })
-export class HoneyMoonComponent { }
+export class HoneyMoonComponent {
+  gifts = [
+    {
+      image: "https://media.bizj.us/view/img/6926112/screen-shot-2014-06-25-at-122653-pm750xx977-550-0-78*750xx750-422-0-0.png",
+      title: "Almoço Shake Shack",
+      content: "Conteúdo",
+      price: "100,00"
+    }
+  ];
+}
