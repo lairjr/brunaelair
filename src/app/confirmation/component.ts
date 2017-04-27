c383bbe2e1aed5b2ac32b1fc7f3cb63066fffd84
lairjr <lairjr@outlook.com>
2017-04-27 18:44:36 -0300

    Add logic to handle form information

--- src/app/confirmation/component.ts
+++ src/app/confirmation/component.ts
@@ -8,14 +8,21 @@ export class ConfirmationComponent {
     name: '',
     email: '',
     guests: [
-      ''
-    ]
+    ],
+    newGuest: ''
   };
   saveInfo = '';
-  addGuest() {
-    this.info.guests.push('');
-  }
-  save() {
+  save(formInfo) {
     this.saveInfo = 'Obrigado, aguardamos você!';
+  }
+  addGuest(newGuest) {
+    this.info.guests.push(newGuest);
+    this.info.newGuest = '';
+  }
+  removeGuest(guestName) {
+    const index = this.info.guests.indexOf(guestName, 0);
+    if (index > -1) {
+       this.info.guests.splice(index, 1);
+    }
   }
 }
