b655144bd094ab7a11294d209464cdd3392c8e06
lairjr <lairjr@outlook.com>
2017-05-02 17:42:57 -0300

    Add more gifts

--- src/app/honeymoon/component.ts
+++ src/app/honeymoon/component.ts
@@ -6,8 +6,80 @@ import { Component } from '@angular/core';
 export class HoneyMoonComponent {
   gifts = [
     {
-      image: "https://media.bizj.us/view/img/6926112/screen-shot-2014-06-25-at-122653-pm750xx977-550-0-78*750xx750-422-0-0.png",
+      image: "http://seoulspace.co.kr/wp-content/uploads/2016/07/shake-shack.jpg",
       title: "Almoço Shake Shack",
+      content: "Conteúdo",
+      price: "100,00"
+    },
+    {
+      image: "https://media-cdn.tripadvisor.com/media/photo-s/09/64/03/b9/ristorante-al-mare.jpg",
+      title: "Jantar no al Mare",
+      content: "Conteúdo",
+      price: "100,00"
+    },
+    {
+      image: "http://www.magnoliabakery.com/wp-content/uploads/2013/12/Magnolia-Bakery-Dubai-Store.jpg",
+      title: "Café na Magnolia Bakery",
+      content: "Conteúdo",
+      price: "100,00"
+    },
+    {
+      image: "https://s-media-cache-ak0.pinimg.com/originals/57/fc/36/57fc36084cdf11355d1e08929a703dc4.jpg",
+      title: "Ingresso roda gigante em Santa Mônica",
+      content: "Conteúdo",
+      price: "100,00"
+    },
+    {
+      image: "http://www.westland.net/venice/images/birdseye.jpg",
+      title: "Drinks na praia de Venice Beach",
+      content: "Conteúdo",
+      price: "100,00"
+    },
+    {
+      image: "https://media-cdn.tripadvisor.com/media/photo-s/0b/af/33/3e/diagon-alley.jpg",
+      title: "Tour no Studios da Warner",
+      content: "Conteúdo",
+      price: "100,00"
+    },
+    {
+      image: "http://travel.usnews.com/images/destinations/16/grand_canyon_main_2014_-_shutterstock-kojihirano.jpg",
+      title: "Visita no Grand Canyon",
+      content: "Conteúdo",
+      price: "100,00"
+    },
+    {
+      image: "http://img0.svcdn.lasvegas.com/v3/cache/lasvegas/32EDBB89E244184BBBD471B36DCB28A4.jpg",
+      title: "Ingresso Cirque du Soeil",
+      content: "Conteúdo",
+      price: "100,00"
+    },
+    {
+      image: "http://www.cplosangeles.com/photogallery/large/hotel/los-angeles-california-hotel-exterior-building.jpg",
+      title: "Hotel Los Angeles",
+      content: "Conteúdo",
+      price: "100,00"
+    },
+    {
+      image: "http://cdn.mntm.me/24/e6/d3/The-Top-Five-Las-Vegas-Family-Hotels-24e6d30f4e324ef6b921588fff4b0e87.jpg",
+      title: "Hotel Las Vegas",
+      content: "Conteúdo",
+      price: "100,00"
+    },
+    {
+      image: "http://www.bestourism.com/img/items/big/91/The-United-States-of-America-_Universal-Studio-in-Los-Angeles-California_156.jpg",
+      title: "Ingresso Parque da Universal",
+      content: "Conteúdo",
+      price: "100,00"
+    },
+    {
+      image: "https://3.bp.blogspot.com/-xZQWz9fbhxw/VrA37VDYqAI/AAAAAAAAAoc/mBT4oK7tm0w/s640/comparadores-aluguel-de-carro.jpg",
+      title: "Alguél de carro",
+      content: "Conteúdo",
+      price: "100,00"
+    },
+    {
+      image: "http://adc-usa.com/wp-content/uploads/2013/10/TCF-HB-3520-copy.jpg",
+      title: "Jantar no Cheesecake Factory",
       content: "Conteúdo",
       price: "100,00"
     }
